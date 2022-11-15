2538947081e6abcc945bd196803afaf694d76d55
maxbond <ceo@rf.ru>
2022-11-16 01:31:05 +0300

    add styling and animation

--- src/About.js
+++ src/About.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
+import videoParis from './assets/paris.mp4'
 
 const About = () => {
     return (
 
         <div>
-            <h3>This app is for travel addicted people.</h3>
+            
+            <div className='video'>
+                <video src ={videoParis} autoPlay muted loop></video>
+                <div className="content">
+                    <h3>This app is for travel addicted people.</h3>
+                </div>
+            </div>
+            
         </div>
 
 
