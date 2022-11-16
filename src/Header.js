6a6905bb268e4840a24c9f57281a24175317bbdf
maxbond <ceo@rf.ru>
2022-11-16 22:07:10 +0300

    MVP

--- src/Header.js
+++ src/Header.js
@@ -1,19 +1,26 @@
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 
+
+
 const Header = () => {
     return (
         <div className="title">
+           
+            
             <h1>TravelPal</h1>
                 <div className="underline"></div>
             <div className="homeAbout">    
-            <Link to='/'>
-            <h3 className="home">Home</h3>    
-            </Link>
-            <Link to='/about'>
-            <h3 className="home">About</h3>    
-            </Link>
-            </div>
+                <Link to='/'>
+                    <h3 className="home">Home</h3>    
+                    </Link>   
+                
+                         
+                
+                <Link to='/about'>
+                    <h3 className="home">About</h3>    
+                </Link>
+                </div>
             
         </div>
         
